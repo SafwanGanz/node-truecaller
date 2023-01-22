4c8595b7d1aee659c58a4329fd0ae1a181ad4250
SafwanGanz <67726326+SafwanGanz@users.noreply.github.com>
2023-01-22 23:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const {searchNumber} = require('truecallerjs');
+async function search (number, id) {
+var searchData = {
+    number: number,
+    countryCode: id,
+    installationId: "a1i0N--dBbtOJVI-xzDnrgBQss_LZ-hhHLUVKDQIGdUv67EmgHq24xutl6ew11HU",
+    output: "JSON"
+}
 
+var sn = await searchNumber(searchData);
+
+return sn
+
+}
